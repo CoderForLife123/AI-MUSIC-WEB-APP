cab58a5610c9c7970afc1ba4e6d4eddf5f213df2
Jay Pandya <91810922+CoderForLife123@users.noreply.github.com>
2023-06-21 18:47:13 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = ""
 song2 = ""
+leftWristX = 0;
+leftWristy = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     song1 = loadSound("Undertale OST - Fallen Down.mp3")
@@ -12,6 +16,23 @@ function setup() {
 
     video = createCapture(VIDEO)
     video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Set Up")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x
+        leftWristY = results[0].pose.leftWrist.y
+        rightWristX = results[0].pose.rightWrist.x
+        rightWristY = results[0].pose.rightWrist.y
+    }
 }
 
 function draw() {
